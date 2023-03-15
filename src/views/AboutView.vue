562dafd8dc5cd60b901b3aadad35622c5686b731
1qjc <102451300+1qjc@users.noreply.github.com>
2023-03-16 07:45:23 +0800

    in

---
+++ src/views/AboutView.vue
@@ -0,0 +1,67 @@
+<template>
+  <el-form :model="form" label-width="120px">
+    <el-form-item label="Activity type">
+      <el-checkbox-group v-model="form.type">
+        <el-checkbox label="Online activities" name="type" />
+        <el-checkbox label="Promotion activities" name="type" />
+        <el-checkbox label="Offline activities" name="type" />
+        <el-checkbox label="Simple brand exposure" name="type" />
+      </el-checkbox-group>
+    </el-form-item>
+    <el-form-item label="Resources">
+      <el-radio-group v-model="form.resource">
+        <el-radio label="Sponsor" />
+        <el-radio label="Venue" />
+      </el-radio-group>
+    </el-form-item>
+    <el-form-item label="Activity form"> </el-form-item>
+    <div class="box" style="height: 200px">
+      <el-input v-model="form.desc" type="textarea" />
+      <el-icon><CirclePlus /></el-icon>
+      <el-input v-model="form.desc" type="textarea" />
+      <el-icon><CirclePlus /></el-icon> <el-input v-model="form.desc" type="textarea" />
+      <el-icon><CirclePlus /></el-icon> <el-input v-model="form.desc" type="textarea" />
+      <el-icon><CirclePlus /></el-icon> <el-input v-model="form.desc" type="textarea" />
+      <el-icon><CirclePlus /></el-icon>
+    </div>
+    <el-form-item>
+      <el-button type="primary" @click="onSubmit">Create</el-button>
+      <el-button>Cancel</el-button>
+    </el-form-item>
+  </el-form>
+  <home-view ref="home"></home-view>
+</template>
+
+<script lang="ts" setup>
+import { reactive, ref, onMounted } from 'vue'
+import HomeView from './HomeView.vue'
+const home = ref(null)
+onMounted(() => {
+  console.log(home.value)
+})
+// do not use same name with ref
+const form = reactive({
+  name: '',
+  region: '',
+  date1: '',
+  date2: '',
+  delivery: false,
+  type: [],
+  resource: '',
+  desc: ''
+})
+
+const onSubmit = () => {
+  console.log('submit!')
+}
+</script>
+
+<style>
+.box {
+  padding: 20px;
+  overflow-y: scroll;
+  border: 1px var(--el-border-color) solid;
+  border-radius: var(--el-border-radius-base);
+  margin: 12px 24px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <audio src=""></audio>
+    <video src=""></video>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+const b = { red: 'red' }
+const a = ref('red')
+defineExpose({ a })
+</script>
+<style>
+.hah {
+  display: flex;
+}
+.red {
+  color: red;
+}
+.label {
+  color: var(--el-text-color-primary);
+}
+.text {
+  color: var(--el-text-color-regular);
+  margin-left: 16px;
+  width: 400px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+</style>
