194e4d19074fa049b6d5279e094a13b7336d3c27
1qjc <102451300+1qjc@users.noreply.github.com>
2023-03-25 13:31:37 +0800

    😏

--- src/views/TableVue.vue
+++ src/views/TableVue.vue
@@ -1,18 +1,18 @@
 <template>
   <el-table
     :data="tableData"
-    border
     style="width: 100%"
+    height="500px"
     :cell-style="setStyle"
     :header-cell-style="setHeaderStyle"
   >
-    <el-table-column prop="date" fixed label="Date" width="170" />
+    <el-table-column prop="date" fixed label="Date" width="180" />
     <el-table-column prop="name" label="Name" width="130" />
-    <el-table-column prop="address" label="Address" min-width="210" />
-    <el-table-column prop="date" label="Date" width="170" />
+    <el-table-column prop="address" label="Address" min-width="180" />
+    <el-table-column prop="date" label="Date" width="150" />
     <el-table-column prop="name" label="Name" width="130" />
-    <el-table-column prop="address" label="Address" min-width="210" />
-    <el-table-column prop="date" label="Date" width="170" />
+    <el-table-column prop="address" label="Address" min-width="180" />
+    <el-table-column prop="date" label="Date" width="150" />
     <el-table-column prop="name" label="Name" width="130" />
     <el-table-column prop="address" label="Address" min-width="210" />
   </el-table>
@@ -73,18 +73,47 @@ const tableData = [
   }
 ]
 function setStyle({ row, column, rowIndex, columnIndex }): CellStyle<any> | undefined {
-  return {
-    padding: '12px 30px',
+  console.log({ row, column })
+  const style: CellStyle<any> = {
+    padding: '12px 18px',
     height: '60px',
-    lineHeight: '18px'
+    lineHeight: '18px',
+    color: '#2c2c2c'
   }
+  if (columnIndex === 0) {
+    return {
+      ...style,
+      padding: '12px 18px 12px 48px'
+    }
+  }
+  if (columnIndex === 8) {
+    return {
+      ...style,
+      padding: '12px 48px 12px 18px'
+    }
+  }
+  return style
 }
 function setHeaderStyle({ row, column, rowIndex, columnIndex }): CellStyle<any> | undefined {
-  return {
-    padding: '12px 30px',
+  const style: CellStyle<any> = {
+    padding: '12px 18px',
     height: '42px',
     lineHeight: '18px',
-    backgroundColor: '#F7f7f7'
+    backgroundColor: '#F7F7F7',
+    color: '#000000',
+    textAlign: 'center'
   }
+  if (columnIndex === 0) {
+    return {
+      ...style,
+      padding: '12px 18px 12px 48px'
+    }
+  }
+  return style
 }
 </script>
+<style scoped>
+.el-table :deep(.cell) {
+  line-height: 18px;
+}
+</style>
