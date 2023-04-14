95d89bcd423f7a8088608a4b34f8d99b0029dbc5
1qjc <102451300+1qjc@users.noreply.github.com>
2023-04-14 21:05:24 +0800

    😆

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
   <RouterView />
 </template>
+<script setup lang="ts"></script>
 <style></style>
